c09f989af34575e20fc88839f5e1bc5efcff40ec
Molly O'Quinn <mollyoquinn@MacBook-Pro-37.fios-router.home>
2020-01-23 16:34:47 -0500

    initial commit

---
+++ program1.cpp
@@ -0,0 +1,12 @@
+/* Program #1 - A first C++ Program 
+*/
+
+#include <iostream>
+using namespace std; 
+
+// main() where proggram execution begins. 
+int main()
+{
+    cout << "This is my first C++ program.";
+    return 0;
+}
