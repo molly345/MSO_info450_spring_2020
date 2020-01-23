750aca5b37809d846136f2b7dc882764abfb672a
Molly O'Quinn <mollyoquinn@MacBook-Pro-37.fios-router.home>
2020-01-23 16:40:42 -0500

    Open commit

---
+++ program2.cpp
@@ -0,0 +1,15 @@
+// Program #2 - using a variable 
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    int x; //this declares a variable 
+    x = 1023; //this assigns 1023 to x
+
+    cout << "This program prints out the value of x."; 
+    cout << x; //This displays 1023
+
+    return 0; 
+}
