0e864e191d9f0c6b46a932f4797a8f6f855488ab
Molly O'Quinn <mollyoquinn@MacBook-Pro-37.local>
2020-01-29 18:32:07 -0500

    initial commit

---
+++ week2/practice3.cpp
@@ -0,0 +1,15 @@
+//Write a program that continually accepts a year value from a user and determines if it is a leap year.
+
+//The year can be evenly divided by 4
+//If the year can be divided by 100 it is NOT a leap year, unless
+//The year is also evenly divisible by 400, then it is a leap year.
+//2000 and 2400 are leap years
+//1800 and 1900 are not leap years
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    
+}
