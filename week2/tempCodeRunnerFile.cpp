77be20766a273768299470aeda60ddce559b7eeb
Molly O'Quinn <mollyoquinn@MacBook-Pro-37.local>
2020-01-30 00:24:17 -0500

    initial commit

--- week2/tempCodeRunnerFile.cpp
+++ week2/tempCodeRunnerFile.cpp
@@ -3,28 +3,25 @@ using namespace std;
 
 int main()
 {
-  const int RULE_ONE = 4;
-  const int RULE_TWO = 100; 
-  const int RULE_THREE = 400; 
 
   int year; 
 
   do{
       cout << "Enter a year value: "; 
       cin >> year; 
-      if (!(year % 100 == 0 ) && (year % 400 == 0)) {
-           cout << year << " is a leap year.\n";
+      if (year % 4 ==0){
+      
+      if (!(year % 100 ) && (year % 400 ==0)) {
+        cout << year << " is a leap year.\n"; 
       }
-
-      if (year % 4 ==0) {
-          cout << year << " is a leap year.\n"; 
+      else {
+        cout << year << " is not a leap year.\n"; 
       }
-       else {
-      cout << year << " is not a leap year.\n";      
-       }
+      }
+      else {
+        cout << year << " is a leap year.\n"; 
+      }
   } 
   while (year > 0); 
-
   return 0; 
-
 }
