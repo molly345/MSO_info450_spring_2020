b1113e1eb12fb81646e03f9444730133f2f112a9
molly345 <59983989+molly345@users.noreply.github.com>
2020-02-06 15:13:43 -0500

    Add files via upload

---
+++ week3/build_functions.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+char  get_user_input();
+void to_upper_case(char &letter);
+int main(){
+  char user_input_letter;
+  cout << "Type in a letter grade, lowercase or uppercase." << endl; 
+  if ('0' != (user_input_letter = get_user_input())){
+    to_upper_case(user_input_letter);
+    switch (user_input_letter){
+      case 'A': cout << "Super Great job on an A!" << endl;break;
+      case 'B': cout << "Great job on a B!" << endl;break;
+      case 'C': cout << "Good job on a C!" << endl;break;
+      case 'D': cout << "Keep at it, you can get there." << endl;break;
+      case 'F': cout << "F is for #fail" << endl;break;
+      default : cout << "This is not a valid grade: " << user_input_letter << endl;
+    }
+  }
+  cout << "You have entered 0.  Exiting." << endl; 
+}
+char  get_user_input(){
+  char letter; 
+  cout << "Enter a letter grade: "; 
+  cin >> letter; 
+  
+}
+void to_upper_case(char &letter){
+   if(letter == 'a'){
+      letter = 'A';
+   } else if (letter == 'b'){
+      letter = 'B';
+   } else if (letter == 'c'){
+      letter = 'C';
+   } else if (letter == 'd'){
+      letter = 'D';
+   }else if(letter == 'f'){
+      letter = 'F';
+   }
+   else {
+     cout << "Please enter a valid letter grade.\n";
+   }
+return; 
+}
