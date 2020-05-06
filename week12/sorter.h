c2ca9b2640bf23103d49253cd6a718578bda355c
molly345 <59983989+molly345@users.noreply.github.com>
2020-05-06 12:12:02 -0400

    Update sorter.h

--- week12/sorter.h
+++ week12/sorter.h
@@ -1,1 +1,11 @@
-erfwersgd
+#ifndef SORTER_H
+#define SORTER_H
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream> 
+using namespace std;
+
+void sort_vector(vector<int> *v);
+
+#endif 
