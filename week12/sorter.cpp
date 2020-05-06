7f3df25a80f555890e2f7ee1a9bcc710814aaac9
molly345 <59983989+molly345@users.noreply.github.com>
2020-05-06 12:11:44 -0400

    Update sorter.cpp

--- week12/sorter.cpp
+++ week12/sorter.cpp
@@ -1,1 +1,24 @@
-wefqearfq
+#include "sorter.h"
+#include <iostream> 
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void sort_vector(vector<int> *v){
+  int i, j;
+  vector<int>& local_vector = *v;
+  cout << " : " << &local_vector << endl;
+  int length = local_vector.size();
+  for (i = 0; i < length-1; i++){
+     for (j = 0; j < length-i-1; j++){
+       if (local_vector[j] > local_vector[j+1]){
+         // cout << " Swapping: " << local_vector[j] << " : " << local_vector[i] << endl;
+         int temp = local_vector[j];
+         local_vector[j] = local_vector[j+1];
+         local_vector[j+1] = temp;
+       }
+     }
+  }
+}
+
